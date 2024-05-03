51863b7c8fae180b0292f8f77b48756907e88847
Fresh-Teacher <98003374+Fresh-Teacher@users.noreply.github.com>
2024-05-03 08:19:47 +0200

    change 404

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,33 +1,27 @@
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
+import './style.css'; 
+
 
 import { CenteredFlexBox, FullSizeCenteredFlexBox } from '@/components/styled';
 import { giphy404, messages } from '@/config';
 
 function NotFound() {
   return (
-    <Container sx={{ height: '100%' }}>
-      <FullSizeCenteredFlexBox flexDirection="column">
-        <iframe
-          src={giphy404}
-          width="100%"
-          height="50%"
-          style={{ maxHeight: '60%', maxWidth: '100%' }}
-          frameBorder="0"
-          allowFullScreen
-        />
-        <CenteredFlexBox flexDirection="column">
-          <Typography sx={{ mt: 2 }} variant="h4" color="error">
-            404 Not Found
-          </Typography>
-          <Divider variant="middle" />
-          <Typography variant="h4" sx={{ color: (theme) => theme.palette.info.main }}>
-            {messages[404]}
-          </Typography>
-        </CenteredFlexBox>
-      </FullSizeCenteredFlexBox>
-    </Container>
+    <div className="error">
+    <div className="sky">
+        <h1 style={{ color: "antiquewhite" }}>ERROR!</h1>
+        <h2><span>4</span><span>0</span><span>4</span></h2>
+        <div className="grass"></div>
+        <img src="aeroplane.png" alt="" className="plane" />
+    </div>
+    <div className="field">
+        <h2>Opps...looks like you got lost.</h2>
+        <a href="#">Go Back Home</a>
+    </div>
+</div>
+
   );
 }
 
