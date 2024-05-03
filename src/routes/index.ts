949d7e531133b31a6f5f753ceda43ffdc442ab26
Fresh-Teacher <98003374+Fresh-Teacher@users.noreply.github.com>
2024-05-03 10:33:51 +0200

    removed unused imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,10 @@
-import AddTaskIcon from '@mui/icons-material/AddTask';
-import BugReportIcon from '@mui/icons-material/BugReport';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeIcon from '@mui/icons-material/Home';
-import TerrainIcon from '@mui/icons-material/Terrain';
+import BookIcon from '@mui/icons-material/MenuBook';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import HistoryIcon from '@mui/icons-material/History';
+import MovieIcon from '@mui/icons-material/Movie';
 
 import asyncComponentLoader from '@/utils/loader';
-
 import { Pages, Routes } from './types';
 
 const routes: Routes = {
@@ -19,25 +18,25 @@ const routes: Routes = {
     component: asyncComponentLoader(() => import('@/pages/Page1')),
     path: '/page-1',
     title: 'Lesson Notes',
-    icon: GitHubIcon,
+    icon: BookIcon,
   },
   [Pages.Page2]: {
     component: asyncComponentLoader(() => import('@/pages/Page2')),
     path: '/page-2',
     title: 'Schemes of Work',
-    icon: AddTaskIcon,
+    icon: AssignmentIcon,
   },
   [Pages.Page3]: {
     component: asyncComponentLoader(() => import('@/pages/Page3')),
     path: '/page-3',
     title: 'Past Papers',
-    icon: TerrainIcon,
+    icon: HistoryIcon,
   },
   [Pages.Page4]: {
     component: asyncComponentLoader(() => import('@/pages/Page4')),
     path: '/page-4',
     title: 'Movies',
-    icon: BugReportIcon,
+    icon: MovieIcon,
   },
   [Pages.NotFound]: {
     component: asyncComponentLoader(() => import('@/pages/NotFound')),
